394387d306d655c51886b6d61b7a6a6d6c5bc56a
Jessica Del <50999226+OutOfCache@users.noreply.github.com>
2020-12-03 15:10:55 +0100

    Update main.py
    Kommentare zur Erklärung
    Konstanten temp_switch und hum_switch als Schwellwerte für einfacheres Ändern

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import arctic, pyramid, swamp, atlantis
+# wenn Pfade genommen werden, dann kann man auch die einzelnen Klassen weglassen
+import arctic, pyramid, swamp, atlantis 
 
+# das ist sehr sinnfrei.
+# TODO: ändern in Pfade
+# z.B. scenes = [["pfad/zu/arctic.json", "pfad/zu/atlantis.json"] ... ]
 scenes = [[arctic.Arctic(), atlantis.Atlantis()],
           [pyramid.Pyramid(), swamp.Swamp()]]
 
@@ -7,12 +11,16 @@ scenes = [[arctic.Arctic(), atlantis.Atlantis()],
 temperature = float(input("Temperatur: "))
 humidity = float(input("Luftfeuchtigkeit: "))
 
-if temperature > 20.0:
+# schwellwerte, an denen es von 0 zu 1 ändert
+temp_switch = 20.0
+hum_switch = 20.0
+
+if temperature > temp_switch:
     temperature = 1
 else: 
     temperature = 0
 
-if humidity > 20.0:
+if humidity > hum_switch:
     humidity = 1
 else: 
     humidity = 0
